ca4e0aba9b1666cf7622c0326b98046b21af2292
Miguel Manjarres <54128874+DevTony101@users.noreply.github.com>
2020-11-28 12:56:13 -0500

    Add: Required classess

---
+++ js/logic/SquareVehicle.js
@@ -0,0 +1,24 @@
+class SquareVehicle extends Vehicle {
+  constructor() {
+    super();
+    this.angle = 0;
+  }
+
+  update() {
+    super.update();
+    let m = super.steer(this.target, 100, 1).mag();
+    this.angle += map(m, 0, this.constraints.maxForce, 0, 0.4);
+    // console.log(this.angle);
+  }
+
+  show() {
+    push();
+    noStroke();
+    fill(this.clr);
+    translate(this.loc.x, this.loc.y);
+    rotate(this.angle);
+    rectMode(CENTER);
+    square(0, 0, 2 * this.size);
+    pop();
+  }
+}
---
+++ js/logic/Vehicle.js
@@ -0,0 +1,74 @@
+class Vehicle {
+  constructor() {
+    this.loc = createVector(random(width), random(height));
+    this.target = createVector();
+    this.vel = createVector();
+    this.acc = createVector();
+    this.clr = color(255, random(200), random(255));
+    this.size = 5;
+    this.constraints = {
+      maxSpeed: 10,
+      maxForce: 0.8
+    };
+  }
+
+  setTarget(x, y) {
+    this.target.set(x, y);
+  }
+
+  addSteerBehavior() {
+    let steer = this.steer(this.target, 100, 1);
+    //steer.mult(2);
+    this.applyForce(steer);
+  }
+
+  addFleeBehavior(target) {
+    let flee = this.flee(target, 80, -1);
+    if (flee) {
+      flee.mult(5);
+      this.applyForce(flee);
+    }
+  }
+
+  applyForce(force) {
+    this.acc.add(force);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.loc.add(this.vel);
+    this.acc.mult(0);
+  }
+
+  show() {
+    stroke(this.clr);
+    strokeWeight(this.size);
+    point(this.loc.x, this.loc.y);
+  }
+
+  steer(target, minDistance, dir) {
+    let desired = p5.Vector.sub(target, this.loc);
+    let d = desired.mag();
+    var speed = this.constraints.maxSpeed;
+    if (d < minDistance) {
+      speed = map(d, 0, minDistance, 0, this.constraints.maxSpeed);
+    }
+    desired.setMag(dir * speed);
+    let steer = p5.Vector.sub(desired, this.vel);
+    steer.limit(this.constraints.maxForce);
+    return steer;
+  }
+
+  flee(target, minDistance, dir) {
+    let desired = p5.Vector.sub(target, this.loc);
+    let d = desired.mag();
+    if (d < minDistance) {
+      desired.setMag(dir * this.constraints.maxSpeed);
+      let steer = p5.Vector.sub(desired, this.vel);
+      steer.limit(this.constraints.maxForce);
+      return steer;
+    } else {
+      return undefined;
+    }
+  }
+}
